0a4eeb0ff56cf598c006a072ea7e8c25ecacaa2b
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2012-05-27 20:37:27 -0300

    rails new file-tree-explorer -O -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileTreeExplorer::Application.config.secret_token = '92ea2518e9f3fe72225899a048bd292aad6648b7a467b352b26c595587f92fadba43b5524ecbf4144ef051404d148d668644fa9d8e7d436d4a5cf082bed6f697'
